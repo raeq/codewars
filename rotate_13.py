383b6ce6798e1c28b52690fabc5ba1a89d30049b
Richard Quinn <quinn.richard@gmail.com>
2021-04-02 13:11:54 +0200

    Simplified error handling

--- rotate_13.py
+++ rotate_13.py
@@ -2,31 +2,27 @@ import string
 
 
 def rotate(value, rotation=13, getstring=True):
-    def make_translation(_rotation):
-        try:
-            t = {}
-            for c in string.ascii_lowercase:
-                t[c] = chr((((ord(c) - 97) + _rotation) % 26) + 97)
-            for c in string.ascii_uppercase:
-                t[c] = chr((((ord(c) - 65) + _rotation) % 26) + 65)
-            for d in string.digits:
-                t[d] = str((int(d) + 5) % 10)
+    def _make_translation(_rotation):
+        t = {}
+        for c in string.ascii_lowercase:
+            t[c] = chr((((ord(c) - 97) + _rotation) % 26) + 97)
+        for c in string.ascii_uppercase:
+            t[c] = chr((((ord(c) - 65) + _rotation) % 26) + 65)
+        for d in string.digits:
+            t[d] = str((int(d) + 5) % 10)
 
-        except Exception as e:
-            raise RuntimeError from e
         return t
 
-    def _translate(value):
+    def _translate(_value):
         try:
-            for c in value:
-                yield translation_table.get(c, c)
+            _translation_table = _make_translation(rotation)
+            for c in _value:
+                yield _translation_table.get(c, c)
         except Exception as e:
             raise RuntimeError from e
 
-    def _translate_to_string(value):
-        return "".join(_translate(value))
-
-    translation_table = make_translation(rotation)
+    def _translate_to_string(_value):
+        return "".join(_translate(_value))
 
     if getstring:
         return _translate_to_string(value)
