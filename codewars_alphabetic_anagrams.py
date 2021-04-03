4bcf3730f4fe91d35027dfdf0b16e182177f9382
Richard Quinn <quinn.richard@gmail.com>
2021-04-03 18:37:13 +0200

    Initial commit, fails timed test obviously

---
+++ codewars_alphabetic_anagrams.py
@@ -0,0 +1,29 @@
+"""
+https://www.codewars.com/kata/53e57dada0cb0400ba000688/train/python
+"""
+from itertools import permutations
+
+
+"""
+Sample words, with their rank:
+ABAB = 2
+AAAB = 1
+BAAA = 4
+QUESTION = 24572
+BOOKKEEPER = 10743
+"""
+
+
+def listPosition(word):
+    """Return the anagram list position of the word"""
+    perms = set()
+
+    for t in permutations(word, len(word)):
+        perms.add("".join(t))
+
+    return sorted(perms).index(word) + 1
+
+
+testValues = {'ABAB': 2, 'A': 1, 'AAAB': 1, 'BAAA': 4, 'QUESTION': 24572, 'BOOKKEEPER': 10743}
+for word in testValues:
+    assert listPosition(word) == testValues[word], f"{word} != {testValues[word]}"
