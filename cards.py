acc353f52e21d78fdd2ff04ddd5457434eaa26e9
Richard Quinn <quinn.richard@gmail.com>
2021-11-30 01:23:24 +0100

    Minor improvements.

--- cards.py
+++ cards.py
@@ -3,6 +3,19 @@ from typing import NamedTuple
 
 
 class Card(NamedTuple):
+    """
+    Represents a playing card.
+    The suit is usually hearts, diamonds etc.
+    The face is usually J, 10, A K, 3, 4 etc
+    The face value is usually the number of pips or standard expectations of a card's value
+    which can change according to the rules of a card game.
+
+    >>> c:Card = Card(suit="Spades", face="A", face_value=11)
+    >>> print(c)
+    Card(suit='Spades', face='A', face_value=11)
+    >>> print(c.description)
+    A of Spades (11)
+    """
     suit: str
     face: str
     face_value: int
@@ -13,17 +26,49 @@ class Card(NamedTuple):
 
 
 class Pack:
+    """
+    A pack, or "deck", of cards. A standard pack has 52 cards, and is also
+    known as a FrenchDeck.
+
+    This class is abstract.
+    """
     suits: list
     cards: list
     faces: list
 
     def __init__(self):
+        self.build_pack()
+
+    def build_pack(self):
+        self.cards = []
         for suit in self.suits:
             for face in self.faces:
                 self.cards.append(Card(suit=suit, face=face[0], face_value=face[1]))
 
     def __repr__(self):
         return f'{self.__class__.__name__}(suits={repr(self.suits)},faces={repr(self.faces)})'
+
+    def remove_card(self, card: Card):
+        self.cards.remove(card)
+
+    def remove_random_facecard(self, face: str):
+        all_faces: list = [c for c in self.cards if c.face == face]
+        self.cards.remove(random.choice(all_faces))
+
+    def add_jokers(self, quantity: int = 1):
+        self.cards.append(Card(suit="Joker", face="JOKER", face_value=0))
+
+    def remove_all_faces(self, faces: list):
+        removed_faces: list = []
+        faces = set(faces)
+
+        for f in self.faces:
+            if f[0] in faces:
+                removed_faces.append(f)
+
+        for f in removed_faces:
+            self.faces.remove(f)
+        self.build_pack()
 
 
 class GermanPack(Pack):
@@ -64,7 +109,7 @@ class Talon:
     cards: list
     shuffled: bool = False
 
-    def __init__(self, packs: int = 1, pack_choice=FrenchPack):
+    def __init__(self, packs: int = 1, pack_choice=FrenchPack()):
         self.cards = []
         self._dealt = 0
 
@@ -73,15 +118,10 @@ class Talon:
 
         self._packs = packs
 
-        if type(pack_choice) not in {type(FrenchPack), type(ItalianPack), type(GermanPack)}:
-            raise ValueError(f"Expected one of "
-                             f"{FrenchPack, ItalianPack, GermanPack}, not {pack_choice!r}")
-
         self._pack_choice = pack_choice
 
         for _ in range(self._packs):
-            self.cards.extend(self._pack_choice().cards[::])
-        self.shuffle()
+            self.cards.extend(self._pack_choice.cards[::])
 
     def shuffle(self):
         for _ in range(7):
@@ -107,8 +147,42 @@ class Talon:
                f'dealt={self.dealt})'
 
 
-t = Talon(-1, pack_choice=FrenchPack)
+# Build Agram Pack https://bicyclecards.com/how-to-play/agram/
+# The kings, queens, jacks, the 2s of all suits and the ace of spades are removed from the deck. The cards of each suit
+# rank, from high to low: A, 10, 9, 8, 7, 6, 5, 4, 3. Because the ace of spades (called "Chief"') is removed from the
+# deck, the highest card in the spade suit is the 10.
+
+agram = FrenchPack()
+agram.remove_all_faces(["K", "Q", "J", "2"])
+agram.remove_card(Card(suit='Spades', face='A', face_value=11))
+
+t = Talon(-1, pack_choice=agram)
+t.shuffle()
+
 print(t)
-while t.cards:
-    print(t.draw(), end='\r')
-print(t)
+for c in t.cards:
+    print(c)
+
+# Build Old Maid Pack https://bicyclecards.com/how-to-play/old-maid/
+# The standard 52-card pack is used, however, one of the four queens is removed, leaving a total of 51 cards.
+
+oldmaid = FrenchPack()
+oldmaid.remove_random_facecard("Q")
+
+t = Talon(-1, pack_choice=oldmaid)
+
+print(len(t.cards))
+for c in t.cards:
+    print(c)
+
+# Build the Continental Rummy Deck
+# Use two 52-card decks and add in 1 Joker per pack, for a total of 106 cards
+
+cont_rummy = FrenchPack()
+cont_rummy.add_jokers(1)
+
+t = Talon(packs=2, pack_choice=cont_rummy)
+
+print(len(t.cards))
+for c in t.cards:
+    print(c)
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -21,30 +21,30 @@ rules = {
 }
 
 winner = {-1: 'The computer wins.', 0: 'You tied.', 1: 'You win.'}
-
-game_history = []
+hand_history = []  # store -1, 0 or 1 each hand
 guess_options = ["rock", "paper", "scissors"]
 
 while True:
     computer_guess = random.choice(guess_options)
-    player_guess = "rock"  # input("Make your guess: ")
+    player_guess = input("Make a guess: ").lower()
 
     if player_guess not in guess_options:
-        print("Invalid guess, try again.")
+        print("Invalid data entry, try again.")
         continue
 
-    game_history.append(rules.get(player_guess).get(computer_guess))
-    msg = f"[{len(game_history)}]: You chose <{player_guess}>, the computer chose <{computer_guess}>."
+    hand_history.append(rules.get(player_guess).get(computer_guess))
+    msg = f"[{len(hand_history)}]: You chose <{player_guess}>, the computer chose <{computer_guess}>."
+    print(f"{msg} {winner[hand_history[-1]]}")
 
-    print(f"{msg} {winner[game_history[-1]]}")
-
-    c = Counter(game_history)
+    c = Counter(hand_history)
 
     if any((x > 1 for k, x in c.items() if k != 0)):
+
         winner = "You won."
         if c[-1] > 1:
             winner = f"The computer won."
-        print(f"\n=======\nGame ends after {len(game_history)} rounds. ")
+        print(f"\n=======\nGame ends after {len(hand_history)} rounds. ")
         print(f"There were {c[0]} ties, computer won {c[-1]} hands, you won {c[1]} hands.\n"
               f"{winner} ")
+
         break
